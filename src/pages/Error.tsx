f65efa72415b02fe0f08b1b73089f05ec024bac0
Ian <adolfvvv567@gmail.com>
2022-12-05 21:05:54 +0300

    error memo

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import {Link} from "react-router-dom";
 
 import Hero from "../Components/Hero/Hero";
@@ -6,7 +6,7 @@ import Banner from "../Components/Banner/Banner";
 import s from '../assets/styles/commonStyles.module.scss';
 
 
-const Error: React.FC = () => {
+const Error: React.FC = memo(() => {
 
     return (
         <Hero>
@@ -17,6 +17,6 @@ const Error: React.FC = () => {
             </Banner>
         </Hero>
     );
-}
+})
 
 export default Error;
